d9af9bcdac8e4812e15f17a979239e57cc64d626
unknown <sreejaray2004@gmail.com>
2026-02-23 22:53:39 +0530

    comitted

---
+++ templates/index.js
@@ -0,0 +1,34 @@
+let questions = [];   // stores all questions
+
+function addQuestion(){
+    let q = document.getElementById("questionBox").value;
+
+    if(q.trim() === "") return;
+
+    questions.push(q);
+
+    // show preview
+    let li = document.createElement("li");
+    li.textContent = q;
+    document.getElementById("preview").appendChild(li);
+
+    document.getElementById("questionBox").value="";
+}
+
+function sendAll(){
+
+    fetch("/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            questions: questions
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert("Server response: " + data.message);
+    })
+    .catch(err => console.log(err));
+}
